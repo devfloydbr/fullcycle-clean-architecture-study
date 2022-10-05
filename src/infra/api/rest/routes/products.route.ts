8986181a03a66efb94612bfc169991ebbac353b9
Guilherme J. Floyd <guilherme.jeronimo@wabtec.com>
2022-10-05 11:41:07 -0300

    BUG

--- src/infra/api/rest/routes/products.route.ts
+++ src/infra/api/rest/routes/products.route.ts
@@ -22,6 +22,7 @@ productsRouter.post('/', async (req: Request, res: Response) => {
 
     res.status(200).send(output)
   } catch (err) {
+    console.log(err)
     res.status(500).send(err)
   }
 })
