5b699378a4bf09d7fbfc9a827a8a207da74d8b85
Guilherme J. Floyd <guilherme.jeronimo@wabtec.com>
2022-10-05 11:34:38 -0300

    BUG

--- src/infra/api/rest/server.ts
+++ src/infra/api/rest/server.ts
@@ -3,4 +3,4 @@ import { app } from './express'
 
 const port: number = Number(process.env.PORT) || 3333
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port} 🚀`))
